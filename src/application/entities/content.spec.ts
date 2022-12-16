1ee027fc549902550dafdc485e8a6663c2845950
Gustavo Inkotte <gus.inkotte@gmail.com>
2022-12-15 21:56:05 -0300

    Content and notification tests

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('Teste de conteúdo.');
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Teste de conteúdo.');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('it should not be able to create a notification content with less than 5 characters', () => {
-  expect(() => new Content('Test')).toThrow();
-});
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Test')).toThrow();
+  });
 
-test('it should not be able to create a notification content with more than 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
